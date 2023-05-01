c0e5fd81c14aa004c41b42eea1b33a38bcd84738
dever-cube <developed.by.fox@gmail.com>
2023-05-01 16:15:27 +0200

    Initial commit

---
+++ src/main/java/net/devcube/nitwitification/Nitwitification.java
@@ -0,0 +1,5 @@
+package net.devcube.nitwitification;
+
+public class Nitwitification {
+
+}
---
+++ src/main/java/net/devcube/nitwitification/client/NitwitificationClient.java
@@ -0,0 +1,5 @@
+package net.devcube.nitwitification.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class NitwitificationClient {
+}
